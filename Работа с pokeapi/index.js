c2c052ebe764e6294924077c6b35cbdf0345c0f4
IlushaKAI <viktorovia@stud.kai.ru>
2025-02-08 14:28:22 +0300

    Добавил комментарии к файлам api

--- Работа с pokeapi/index.js
+++ Работа с pokeapi/index.js
@@ -1,19 +1,19 @@
 async function fetchData() {
     try{
-        const pokemonName = document.getElementById('pokemonName').value.toLowerCase()
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-        if(!response.ok){
+        const pokemonName = document.getElementById('pokemonName').value.toLowerCase() // получаем значения из инпут поля
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`) // асинхронный запрос fetch с проверкой
+        if(!response.ok){ // если запрос не завершиля с кодом ok, то выбрасываем ошибку
             throw new Error("Не удалось достучаться до ресурса")
         }
 
-        const data = await response.json()
-        const pokemonSprite = data.sprites.front_default
-        const imgElement = document.getElementById('pokemonSprite')
-        imgElement.src = pokemonSprite
-        imgElement.style.display = "block"
+        const data = await response.json() // ожидаем ответа и присваиваем этот ответ переменной data
+        const pokemonSprite = data.sprites.front_default // обращаемся к содержимому json файла, который записан в переменную data -> получаем по сути ссылку на sprite и присваиваем ее переменной 
+        const imgElement = document.getElementById('pokemonSprite') // обращаемся к DOM модели и получаем элемент с id pokemonSprite
+        imgElement.src = pokemonSprite // присваиваем этому элементу ссылку на sprite
+        imgElement.style.display = "block" // меняем свойство display на block, чтобы картинка отобразилась
         console.log(data)
     }
-    catch(error){
+    catch(error){ // если произошла ошибка, то выводим ее в консоль
         console.error(error)
     }
     
